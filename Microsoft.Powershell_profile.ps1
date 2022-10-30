3d56d2ff10a0666f33fd1dc77a657955b10c0441
Bacskay Viktor <bacskay.viktor@gmail.com>
2022-10-30 16:07:24 +0100

    remove redundant stuff add steam to additional install

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,13 +1,7 @@
 Import-Module PSReadLine
 oh-my-posh init pwsh --config "$HOME\vikesz-posh.omp.json" | Invoke-Expression
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-# PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
+
 function Set-GitConfig-Work {
     git config user.name "Bacskay Viktor"
     git config user.email "bacskay.viktor@autsoft.hu"
